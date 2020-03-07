1df7e75c0e50715b14e9547a0a91f9b10c9b68b6
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-07 13:58:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,28 @@ class ApplicationController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 
-  get '/' do
+  get '/artists' do
+    @all = Artist.all
     erb :index
   end
+
+  get '/artists/:slug' do
+    @artist = Artist.fing_by_slug(params[:slug])
+    erb :'artist/show'
+  end
+
+
+
+  # get '/:class_name' do
+  #   class_name = params[:class_name].singularize.capitalize
+  #   @all = send("#{class_name}.all")
+  #   erb :index
+  # end
+  #
+  # get '/:class_name/:slug' do
+  #   class_name = params[:class_name].singularize.capitalize
+  #   @instance = send("#{class_name}.find_by_slug(#{params[:slug]})")
+  #   erb :'#{class_name.downcase}/show'
+  # end
+
 end
