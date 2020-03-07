31cf428c05b36bc7938d7eb857b478021c5e742c
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-07 15:02:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,6 +51,8 @@ class ApplicationController < Sinatra::Base
 
   get '/songs/:slug/edit' do
     @song = Song.find_by_slug(params[:slug])
+    @artists = Artist.all
+    @genres = Genre.all
     erb :'song/edit'
   end
 
